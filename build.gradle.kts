1da892bd114c50e5a3e9eb3c6f3606e821390d64
yazicihamit4-tech <yazicihamit4@gmail.com>
2026-02-23 23:33:03 +0300

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+
+    // Eğer sadece Yandex Ads kullanıyorsan ve Firebase kullanmıyorsan bu satırı silebilirsin.
+    // Firebase kullanıyorsan kalsın:
+    id("com.google.gms.google-services") version "4.4.0" apply false
+}
